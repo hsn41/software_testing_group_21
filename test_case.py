e183e5e581d2545c6c15cd89c2de512a93fe4a39
Hassan Abbas <hsnch41@gmail.com>
2019-12-10 05:47:27 +0500

    i have updated cases for my task
    everyone can add their test case

--- test_case.py
+++ test_case.py
@@ -1,15 +1,29 @@
-import numpy.testing as np
-from numpy.core import(
-     intp, float32, empty, arange, array_repr, ndarray, isnat, array)
+from __future__ import division, absolute_import, print_function
 import unittest
+import numpy as np
+from numpy import array,dot
+from numpy.linalg import matrix_power
+from numpy import linalg as LinAlgError
+
 class TestClass(unittest.TestCase):
-    """ Functions for testing. """
+    # we have to use these matrix for further test cases,you can use a,b,c and perform functions
     def setUp(self):
+          
         self.value_a = 2.3333333333333
         self.value_b = 2.3333333333333
         self.sample_array = [1.0,2.3333333333333]
+     
+        self.a = array([[2,2,1],[5,3,4],[2,3,6]])
+        self.b = array([[4,2,3],[6,1,7],[2,4,6]])
+        self.c = array([[6,3,5],[7,2,9],[1,3,5]])
+          
         self.x = [1e-5, 1e-3, 1e-1]
         self.y = np.arccos(np.cos(self.x))
+         # our test cases should be equal to these cases
+        self.dot_of_ab = array([[22, 10, 26], [46, 29, 60], [38, 31, 63]])
+        self.power_of_a = array([[16, 13, 16], [33, 31, 41], [31, 31, 50]])
+        self.inverse_of_a = array([[-6/23,9/23,-5/23],[22/23,-10/23,3/23],[-9/23,2/23,4/23]])
+        self.multi_dot_equal = array([[228,164,330],[539,376,791],[508,365,784]])
 
 
     # to check given value is nearly equal to desired result or not
@@ -29,14 +43,28 @@ class TestClass(unittest.TestCase):
         np.assert_string_equal("hello\nmultiline", "hello\nmultiline")
 
    # i dont know why i cant use array,can you please guide what are the mistake i am making
-    def test_equal_nan(self):
-        a = np.array([np.nan])
-        b = np.array([np.nan])
-        # Should not raise:
-        np.assert_allclose(a, b, equal_nan=True)
+#     def test_equal_nan(self):
+#         a = np.array([np.nan])
+#         b = np.array([np.nan])
+#         # Should not raise:
+#         np.assert_allclose(a, b, equal_nan=True)
 
-   # def test_cos(self):
-   #     np.testing.assert_allclose(self.x, self.y, rtol=1e-5, atol=0)
+  # dot product of matrix
+    def test_two_array(self):
+        np.testing.assert_equal(dot(self.a,self.b),self.dot_of_ab)
+
+    # multiple dot of matrix
+    def test_multidot_matrix(self):
+        np.testing.assert_equal(dot(dot(self.a,self.b),self.c),self.multi_dot_equal)
+    # inverse of matrix
+    def test_inverse_matrix(self):
+        np.testing.assert_almost_equal(LinAlgError.inv(self.a),self.inverse_of_a)
+
+    # matrix power for power 2
+    def test_matrix_power(self):
+        np.testing.assert_equal(matrix_power(self.a,2),self.power_of_a)
+
+   
 
 
 if __name__ == "__main__":
